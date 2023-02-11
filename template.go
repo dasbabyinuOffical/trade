8537c0434448ed3933a1f37b7a703fcd1aade236
KevenNock <KevenNock@gmail.com>
2023-02-11 13:21:42 +0800

    fix template

--- template.go
+++ template.go
@@ -1,6 +1,6 @@
 package main
 
 const (
-	templateUp   = "%s快速上涨%.1f%%,之前价格:%.1f,当前价格:%.1f,时间:%s"
-	templateDown = "%s快速上涨%.1f%%,之前价格:%.1f,当前价格:%.1f,时间:%s"
+	templateUp   = "%s快速上涨%f%%,之前价格:%f,当前价格:%f,时间:%s"
+	templateDown = "%s快速上涨%f%%,之前价格:%f,当前价格:%f,时间:%s"
 )
